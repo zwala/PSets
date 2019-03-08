7b0228bb1a414ef85dafd85c17231b9e4df6ce13
Zwala <46795160+zwala@users.noreply.github.com>
2019-03-08 16:55:45 -0500

    Add files via upload

---
+++ hand.py
@@ -0,0 +1,104 @@
+import random 
+
+class Hand(object):
+    def __init__(self, n):
+        '''
+        Initialize a Hand.
+
+        n: integer, the size of the hand.
+        '''
+        assert type(n) == int
+        self.HAND_SIZE = n
+        self.VOWELS = 'aeiou'
+        self.CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
+
+        # Deal a new hand
+        self.dealNewHand()
+
+    def dealNewHand(self):
+        '''
+        Deals a new hand, and sets the hand attribute to the new hand.
+        '''
+        # Set self.hand to a new, empty dictionary
+        self.hand = {}
+
+        # Build the hand
+        numVowels = self.HAND_SIZE // 3
+    
+        for i in range(numVowels):
+            x = self.VOWELS[random.randrange(0,len(self.VOWELS))]
+            self.hand[x] = self.hand.get(x, 0) + 1
+        
+        for i in range(numVowels, self.HAND_SIZE):    
+            x = self.CONSONANTS[random.randrange(0,len(self.CONSONANTS))]
+            self.hand[x] = self.hand.get(x, 0) + 1
+            
+    def setDummyHand(self, handString):
+        '''
+        Allows you to set a dummy hand. Useful for testing your implementation.
+
+        handString: A string of letters you wish to be in the hand. Length of this
+        string must be equal to self.HAND_SIZE.
+
+        This method converts sets the hand attribute to a dictionary
+        containing the letters of handString.
+        '''
+        assert len(handString) == self.HAND_SIZE, "Length of handString ({0}) must equal length of HAND_SIZE ({1})".format(len(handString), self.HAND_SIZE)
+        self.hand = {}
+        for char in handString:
+            self.hand[char] = self.hand.get(char, 0) + 1
+
+
+    def calculateLen(self):
+        '''
+        Calculate the length of the hand.
+        '''
+        ans = 0
+        for k in self.hand:
+            ans += self.hand[k]
+        return ans
+    
+    def __str__(self):
+        '''
+        Display a string representation of the hand.
+        '''
+        output = ''
+        hand_keys = sorted(self.hand.keys())
+        for letter in hand_keys:
+            for j in range(self.hand[letter]):
+                output += letter
+        return output
+
+    def update(self,word):
+        hand_keys=sorted(self.hand.keys())
+        str_hand=""
+        long=len(word)
+        #Converts the hand dictionery to string
+        for letter in hand_keys:
+            for j in range(self.hand[letter]):
+                str_hand+=letter   
+        
+        list_hand=list(str_hand)
+        count=0
+        for each in word:
+            if each in list_hand:
+                list_hand.remove(each)
+                count+=1
+        if count==long: 
+            #update the hand
+            di={}
+            for each in list_hand:
+                if each in di.keys():
+                    di[each]+=1
+                else:
+                    di[each]=1
+            self.hand=di
+            return True
+        else:
+            return False
+            
+
+myHand=Hand(8)
+myHand.setDummyHand('shoelace')
+print (myHand)
+print (myHand.update('shoe'))
